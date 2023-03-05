c99cfe5e2cbae9d52e8ef24f708010e37862ca26
nadikkk <109106413+nadikkk@users.noreply.github.com>
2023-03-05 12:53:10 +0200

    pretierr

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import css from './Searchbar.module.css';
 import { FiSearch } from 'react-icons/fi';
 
-export default function Searchbar ({onSubmit}) {
-	const [nameImg, setNameImg] = useState('')
- 
+export default function Searchbar({ onSubmit }) {
+  const [nameImg, setNameImg] = useState('');
+
   const isSubmit = e => {
     e.preventDefault();
     if (nameImg.trim() === '') {
@@ -14,29 +14,28 @@ export default function Searchbar ({onSubmit}) {
     onSubmit(nameImg);
   };
   const changeNameImg = e => {
-	setNameImg( e.currentTarget.value.toLowerCase().trim());
+    setNameImg(e.currentTarget.value.toLowerCase().trim());
   };
-  
-	return (
-	<header className={css.searchbar}>
-		<form className={css.form} onSubmit={isSubmit}>
-			<button type="submit" className={css.button}>
-			<FiSearch />
-			</button>
 
-			<input
-			className={css.input}
-			type="text"
-			autoComplete="off"
-			autoFocus
-			placeholder="Search images and photos"
-			value={nameImg}
-			onChange={changeNameImg}
-			/>
-		</form>
-	</header>
-	);
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form} onSubmit={isSubmit}>
+        <button type="submit" className={css.button}>
+          <FiSearch />
+        </button>
 
+        <input
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={nameImg}
+          onChange={changeNameImg}
+        />
+      </form>
+    </header>
+  );
 }
 
 Searchbar.propTypes = {
